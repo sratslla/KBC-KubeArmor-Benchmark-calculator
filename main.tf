99eb27e1653942cc08ee07343c3f5490e6381b5b
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-05-27 12:31:20 +0530

    node count updated

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ provider "google" {
 resource "google_container_cluster" "primary" {
   name               = "example-cluster"
   location           = "us-central1-c"
-  initial_node_count = 0
+  initial_node_count = 1
 
   node_config {
-    machine_type = "e2-standard-4"
+    machine_type = "e2-custom-2-4096"
   }
 }
 
 resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.primary.name
   location   = google_container_cluster.primary.location
-  node_count = 3
+  node_count = 2
 
   node_config {
     machine_type = "e2-custom-2-4096"
