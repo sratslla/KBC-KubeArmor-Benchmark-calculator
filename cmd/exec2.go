2d4f6adfbd31df05c2c48719c19edd940e7e8d6a
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-07-08 11:20:21 +0530

    exec2 added

--- cmd/exec2.go
+++ cmd/exec2.go
@@ -4,7 +4,10 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -16,9 +19,52 @@ var exec2Cmd = &cobra.Command{
 	Long:  `This will keep the replicaset constant and install kubearmor.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("exec2 called")
+
+		deployments := []string{
+			"cartservice",
+			"currencyservice",
+			"emailservice",
+			"checkoutservice",
+			"frontend",
+			"paymentservice",
+			"productcatalogservice",
+			"recommendationservice",
+			"redis-cart",
+			"shippingservice",
+			"adservice",
+		}
+
+		replicasMap := make(map[string]int)
+
+		for _, deployment := range deployments {
+			replicas, err := getCurrentReplicas(deployment)
+			if err != nil {
+				fmt.Printf("Error getting current replicas %s : %v\n", deployment, err)
+				continue
+			}
+			replicasMap[deployment] = replicas
+		}
+		for deployment, replicas := range replicasMap {
+			fmt.Printf("%s: %d replicas\n", deployment, replicas)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(exec2Cmd)
+}
+func getCurrentReplicas(deploymentName string) (int, error) {
+	cmd := exec.Command("kubectl", "get", "hpa", deploymentName, "-o", "jsonpath={.status.currentReplicas}")
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	err := cmd.Run()
+	if err != nil {
+		return 0, fmt.Errorf("error getting the current replicas %V \n%s", err, output.String())
+	}
+	var replicas int
+	err = json.Unmarshal(output.Bytes(), &replicas)
+	if err != nil {
+		return 0, fmt.Errorf("error unmarshalling current replicas: %v", err)
+	}
+	return replicas, nil
 }
